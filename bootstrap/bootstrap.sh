619d75c6b6224d15a5f8f1109e33e6b37ce63106
Tom Regan <code.tom.regan@gmail.com>
2013-03-01 23:13:29 +0000

    adds bootstrap script

---
+++ bootstrap/bootstrap.sh
@@ -0,0 +1,18 @@
+[[ "root" != $(whoami) ]] && echo "not root" && exit
+
+packages=(
+    "http://downloads.puppetlabs.com/mac/facter-1.6.17.dmg"
+    "http://downloads.puppetlabs.com/mac/hiera-1.1.2.dmg"
+    "http://downloads.puppetlabs.com/mac/puppet-3.1.0.dmg"
+)
+
+for package in ${packages[@]}; do
+    FILENAME=$(basename ${package})
+    [[ ! -f $FILENAME ]] && curl -O $package 2> /dev/null
+    [[ ! -d "/Volumes/${FILENAME/\.dmg/}" ]] && open $FILENAME
+    sleep 2 # short wait required before unmount
+    which ${FILENAME%%-*\.dmg} > /dev/null
+    [[ $? != 0 ]] && installer -pkg /Volumes/${FILENAME/\.dmg}/${FILENAME/\.dmg/}.pkg -target / > /dev/null
+    hdiutil unmount /Volumes/${FILENAME/\.dmg/} -force > /dev/null
+    [[ -f $FILENAME ]] && rm $FILENAME
+done
